489535426b5fd9f6b92155ba93d70a78ed4a4f0b
OHTA Shogo <athos0220@gmail.com>
2012-04-24 22:07:52 +0900

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: lein run -m bfmaker.core $PORT
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject bfmaker "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [compojure "0.6.5"]
+                 [ring/ring-jetty-adapter "0.3.11"]
+                 [fleet "0.9.5"]
+                 [congomongo "0.1.7"]])
